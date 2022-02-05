456f1fd4d6aeacc10a037e38e5dd3401f3e79ec6
unknown <LENOVO@RCSLPT137.corp.rapyder.com>
2022-02-05 23:53:15 +0530

    palindrome

---
+++ middleIndex (leftSum = rightSum)/code.py
@@ -0,0 +1,13 @@
+def findMiddleIndex(self, nums: List[int]) -> int:
+    leftSum = 0
+    rightSum = sum(nums)
+    
+    for i in range(len(nums)):
+        
+        if leftSum == rightSum - nums[i]:
+            return i
+        
+        leftSum += nums[i]
+        rightSum -= nums[i]
+        
+    return -1
---
+++ middleIndex (leftSum = rightSum)/optimized_code.py
@@ -0,0 +1,27 @@
+def findMiddleIndex(self, nums: List[int]) -> int:
+    Prefix_Sum = [0] * len(nums)        #Intialize with 0 and length will be same sa nums
+    
+    Prefix_Sum[0] = nums[0]             #Assign first value of nums to prefix sum
+    
+    for i in range(1,len(nums)):                    # Loop from 1 to length of nums
+        Prefix_Sum[i] = Prefix_Sum[i-1] + nums[i]  # Keep adding previous value to current value
+        
+    for i in range(len(nums)):
+        
+        if i==0:
+            left = 0
+        else:
+            left =  Prefix_Sum[i-1]
+            
+        
+        if i==len(nums)-1:
+            right=0
+        else:
+            right = Prefix_Sum[len(nums)-1] - Prefix_Sum[i]
+            
+            
+        if left == right:
+            return i
+    
+    return -1
+        
